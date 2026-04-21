7e4fd1ebf12cfa14e2dbb50ec59e012184c4dacb
Jae Hyeok Sim <sjh4481@mutecsoft.com>
2026-04-21 17:51:23 +0900

    v1.0.9

--- ax2-chatbot-back/app/core/security.py
+++ ax2-chatbot-back/app/core/security.py
@@ -4,7 +4,8 @@ from app.core.config import settings
 # TODO[sjh] origin .env이나 config.yml파일로 관리
 def cors(app):
     origins = [
-        settings.cors_url,
+        settings.public_cors_url,
+        settings.internal_cors_url,
         "http://localhost:13000",
         "http://localhost:14000",
     ]
